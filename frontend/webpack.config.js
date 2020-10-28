7f277f39da7e5f59f495cb5c0e28831c316715f3
Arslanoov <rasularslanoov@gmail.com>
2020-10-28 15:39:21 +0500

    Installed React and babel

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,7 +4,21 @@ const path = require('path');
 module.exports = {
     mode: "development",
 
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
 
     plugins: [
         new HtmlWebpackPlugin({
